2f2e556fe1a6f6500d6277b81bde1c921f7397ce
Aviv Baron David <bardaviv@gmail.com>
2024-06-26 17:51:52 +0300

    Add NoteNav component

---
+++ apps/note/cmps/NoteNav.jsx
@@ -0,0 +1,14 @@
+const { Link } = ReactRouterDOM
+
+export function NoteNav({ note, onRemove }) {
+
+
+    return (
+        <nav className='note-nav'>
+            <button onClick={() => onRemove(note.id)} >remove</button>
+            {/* <Link to={`/note/${note.id}`}><button>Details</button></Link> */}
+            <Link to={`/note/edit/${note.id}`}><button>edit</button></Link>
+
+        </nav>
+    )
+}
