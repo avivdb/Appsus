8c7dd48a3b722b424f255b1964c112f12d39e06e
Amit Gal <amitgal@hotmail.com>
2024-06-27 01:24:15 +0300

    Fix css logic better ux and functionallity

--- apps/mail/services/mail.service.js
+++ apps/mail/services/mail.service.js
@@ -61,8 +61,8 @@ function convertToDate(timestamp) {
     year: 'numeric',
     month: 'long',
     day: 'numeric',
-    hour: '2-digit',
-    minute: '2-digit',
+    // hour: '2-digit',
+    // minute: '2-digit',
   }
   return date.toLocaleDateString(undefined, options)
 }
