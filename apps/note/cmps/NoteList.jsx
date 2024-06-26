b8b94d3a46cd6297f11a1ffb0e0d263c4789a0c2
Aviv Baron David <bardaviv@gmail.com>
2024-06-26 14:13:10 +0300

    Update NoteIndex,NoteList,NotePreview

--- apps/note/cmps/NoteList.jsx
+++ apps/note/cmps/NoteList.jsx
@@ -1,8 +1,24 @@
-export function NoteList() {
+import { NotePreview } from "./NotePreview.jsx"
+
+const { Link } = ReactRouterDOM
+
+
+export function NoteList({ notes, onRemove }) {
 
     return (
         <section className="note-list">
-            note list
+            <ul>
+                {notes.map(note =>
+                    <li key={note.id} >
+                        <NotePreview note={note} />
+                        <button onClick={() => onRemove(note.id)} >remove</button>
+                        <nav className='note-nav'>
+                            <Link to={`/note/${note.id}`}><button>Details</button></Link>
+                            <Link to={`/note/edit/${note.id}`}><button>edit</button></Link>
+                        </nav>
+                    </li>
+                )}
+            </ul>
         </section>
     )
 }
